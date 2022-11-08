a4b4b56884c96232894452862fde905062ec223c
Jeong <63828202+JeongA-Shin@users.noreply.github.com>
2022-11-08 18:37:24 +0900

    [Resolves #6] redis config 설정 및 테스트 코드 (#5)
    * chore : add config for redis
    
    * chore: add config for redis
    
    * test: add test for redis
    
    * feat: 엔티티 작성
    
    - Problem - Level : Problem만 단방향으로 연관 가짐
    - Problem - ProblemCategory - Category : Problem만 ProblemCategory와 양방향 연관 가짐 (카테고리별 문제 조회 필요할 경우에 Category에 연관 설정할 것)
    - Problem 연관 객체 Eager로 가져오도록 설정 (논의 필요)
    
    * feat: Repository 작성
    
    * refactor: 필요 없는 gitkeep 삭제
    
    * feat: 엔티티 작성
    
    https://github.com/KHUCapston-concoder/Backend/issues/1
    
    * refactor : 필요없는 주석 삭제
    
    https://github.com/KHUCapston-concoder/Backend/issues/6
    
    * refactor : 콘솔 확인 삭제
    
    https://github.com/KHUCapston-concoder/Backend/issues/6
    
    Co-authored-by: Subin Min <tnqls5417@naver.com>

--- src/main/java/oncoding/concoder/ConcoderApplication.java
+++ src/main/java/oncoding/concoder/ConcoderApplication.java
@@ -2,7 +2,9 @@ package oncoding.concoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ConcoderApplication {
 
